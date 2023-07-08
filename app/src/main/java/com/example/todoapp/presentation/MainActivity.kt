0dd339da4c666fda14d34ec9a9860e302686bcf0
Nikolai Tsygankov <star-lord3000@mail.ru>
2023-07-08 07:58:45 +0300

    Add docstring

--- app/src/main/java/com/example/todoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/presentation/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.todoapp.R
 
+/**
+ * Single activity in which the fragments are located.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
